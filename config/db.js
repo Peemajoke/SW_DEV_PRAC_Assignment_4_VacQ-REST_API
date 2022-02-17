cc1acc12a205fc6282e0208e3af5079e40a3be94
Peemajoke <zinger.peem@gmail.com>
2022-02-17 18:34:13 +0700

    fix config,env file and install bcryptjs

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({path:'./config.env'});
 
 const connectDB = async ()=>{
     // const conn= await mongoose.connect(process.env.MONGO_URI, {
-    const conn= await mongoose.connect('mongodb+srv://Peem:0858@vacqcluster.5vzfl.mongodb.net/VacQAssignment5?retryWrites=true&w=majority', {
+    const conn= await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         // useUnifiedTopologu: true
     });
